759cf27195506cec0aa0caaf63a4079330e498be
Dhannuka Priyanath Ranasinghe <dhanuka.priyanath@gmail.com>
2015-07-02 00:25:16 +0530

    First commit

---
+++ org.reactor.core.templates/src/main/java/org/reactor/core/templates/App.java
@@ -0,0 +1,13 @@
+package org.reactor.core.templates;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
