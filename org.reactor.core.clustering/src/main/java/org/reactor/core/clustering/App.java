f103a33875bf5b63d6a781618002c137551cd398
Dhannuka Priyanath Ranasinghe <dhanuka.priyanath@gmail.com>
2015-07-02 00:37:16 +0530

    second commit

---
+++ org.reactor.core.clustering/src/main/java/org/reactor/core/clustering/App.java
@@ -0,0 +1,13 @@
+package org.reactor.core.clustering;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
